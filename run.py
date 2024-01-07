30668af24b03ce99ea65564e1fa70869283a6825
Shuuuuaib <91546177+Shuuuuaib@users.noreply.github.com>
2024-01-07 14:11:23 +0100

    Update run.py

--- run.py
+++ run.py
@@ -18,8 +18,8 @@ from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, Streame
 Keep_alive.keep_alive()
 
 twitch_miner = TwitchChannelPointsMiner(
-      username=os.getenv("user"),
-      password=os.getenv("passw"),          # If no password will be provided, the script will ask interactively
+      username=os.environ.get("user"),
+      password=os.environ.get("passw"),          # If no password will be provided, the script will ask interactively
     claim_drops_startup=True,                  # If you want to auto claim all drops from Twitch inventory on the startup
     priority=[                                  # Custom priority in this case for example:
         Priority.STREAK,                        # - We want first of all to catch all watch streak from all streamers
